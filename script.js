631363ec578f7ad15c987d4932382ca47af6730c
Nikai Steur <nikai.steur@iconum.com>
2025-08-06 12:31:40 +0200

    FAD-site v1

---
+++ script.js
@@ -0,0 +1,16 @@
+// Voeg hier je komende optredens toe
+const gigs = [
+
+    { date: "21 september 2025", place: "Floralia, Nieuw Niedorp" },
+    { date: "5 oktober 2025", place: "Dorpshuis, Warmenhuizen" },
+    { date: "10 oktober 2025", place: "Bloemencorso, Winkel" },
+    { date: "30 oktober 2025", place: "Podium Victorie, Alkmaar" },
+    { date: "8 november 2025", place: "Ontmoeting, De Goorn" },
+];
+
+const gigsList = document.getElementById('gigs-list');
+gigs.forEach(gig => {
+    const li = document.createElement('li');
+    li.textContent = `${gig.date} – ${gig.place}`;
+    gigsList.appendChild(li);
+});
